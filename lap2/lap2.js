aba57da4c739c50da149c70379a7c57b0b11b9ec
haolq664 <lehao99nd@gmail.com>
2025-11-05 13:00:44 +0700

    first commit

---
+++ lap2/lap2.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 3; i++) {
+    setTimeout(function () {
+        console.log(i); // Hiện tại in ra 3,3,3
+    }, 100);
+}
+
+// Tạo một object student với const
+// Thêm thuộc tính điểm và in ra
+const student = {
+    name: "ThuyTien",
+    age: 20,
+};
+
+// Thực hiện các thao tác sau:
+// 1. Thêm thuộc tính grade với giá trị "A"
+// 2. Thay đổi tuổi thành 21
+// 3. In ra toàn bộ thông tin student
--- lap2/main.js
+++
@@ -1,5 +0,0 @@
-// Sửa đoạn code sau để hoạt động đúng
-for (var i = 0; i < 3; i++) {
-    console.log(i)
-}
-// Yêu cầu: Sửa để in ra 0,1,2
