79fa514e3c4f9efbfc69baf9ea595c1fe1c6e721
haolq664 <lehao99nd@gmail.com>
2025-11-07 12:33:48 +0700

    lap3

--- lap3/lap3.js
+++ lap3/lap3.js
@@ -1,26 +1,30 @@
 // Function 1
-function multiply(a, b) {
-  return a * b;
-}
-const multiply=(a,b) => a*b;
+// function multiply(a, b) {
+//   return a * b;
+// }
+const multiply1=(a,b) => {
+  return a*b;
+};
+console.log(multiply1(4,5))
+
 // Function 2
-function isPositive(number) {
-  return number >= 0;
-}
+// function isPositive(number) {
+//   return number >= 0;
+// }
 const isPositive=(number)=>{
     return number >=0;
 }
 // Function 3
-function getRandomNumber() {
-  return Math.random();
-}
+// function getRandomNumber() {
+//   return Math.random();
+// }
 const getRandomNumber=()=>{
     return Math.random();
 }
 // Function 4
-document.addEventListener("click", function () {
-  console.log("Clicked!");
-});
+// document.addEventListener("click", function () {
+//   console.log("Clicked!");
+// });
 document.addEventListener("click",()=>{
     console.log("Clicked!");
 });
